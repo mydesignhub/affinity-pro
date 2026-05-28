c1729eaf6bcddf0858051d60a3f80f7e510c656b
mydesign <koymy.mlk@gmail.com>
2026-05-28 09:41:10 +0700

    New app Affinity Pro

--- affinity-ipad/frontend/src/utils/haptics.js
+++
@@ -1,7 +0,0 @@
-export const triggerHaptic = (type = 'light') => {
-    if (typeof navigator !== 'undefined' && navigator.vibrate) {
-        if (type === 'error') navigator.vibrate([50, 50, 50]);
-        else if (type === 'success') navigator.vibrate([30, 50, 30]);
-        else navigator.vibrate(10);
-    }
-};
